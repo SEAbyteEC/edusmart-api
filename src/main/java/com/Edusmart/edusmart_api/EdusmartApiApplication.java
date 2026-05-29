b83c4cc2ea09fbe01896c583b852ee6ef1e9d3c9
SEAbyteEC <stalin.m22@gmail.com>
2026-05-28 20:06:48 -0500

    chore: agregar archivos base del proyecto

---
+++ src/main/java/com/Edusmart/edusmart_api/EdusmartApiApplication.java
@@ -0,0 +1,13 @@
+package com.Edusmart.edusmart_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusmartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusmartApiApplication.class, args);
+	}
+
+}
